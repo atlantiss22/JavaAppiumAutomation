bd62dd98c38b607ba25278edbc44442c1571a3ca
Цилдерманис Эрик <cerik@phoenixit.ru>
2019-11-03 14:23:47 +0300

    Lesson 0 Test-2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -7,4 +7,10 @@ public class MainClassTest {
     public void testGetLocalNumber() {
         Assert.assertEquals("Local number is not equal to", 14, MainClass.getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber() {
+        MainClass mainClass = new MainClass();
+        Assert.assertTrue("Class number <= 45", mainClass.getClassNumber() > 45);
+    }
 }
